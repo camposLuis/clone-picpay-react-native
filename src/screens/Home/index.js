5eccc8507225567a50a700ad42a0d0e154eda3cc
camposLuis <luis.campos.henrique@hotmail.com>
2020-04-21 14:17:08 -0300

    Create Header App

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import {Text} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
-// import { Container } from './styles';
+import Suggestions from '../../components/Suggestions';
+
+import {
+  Wrapper,
+  Container,
+  Header,
+  BalanceContainer,
+  BalanceTitle,
+  Balance,
+} from './styles';
 
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <Wrapper>
+      <Container>
+        <Header>
+          <MaterialCommunityIcons
+            name="qrcode-scan"
+            size={30}
+            color="#10c862"
+          />
+
+          <BalanceContainer>
+            <BalanceTitle>Meu Saldo</BalanceTitle>
+            <Balance>R$ 0,00</Balance>
+          </BalanceContainer>
+
+          <AntDesign name="gift" size={30} color="#10c862" />
+        </Header>
+
+        <Suggestions />
+      </Container>
+    </Wrapper>
+  );
 }
