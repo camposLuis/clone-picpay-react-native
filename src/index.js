bb415a32d9afb0870d6852207606773d78be3850
camposLuis <luis.campos.henrique@hotmail.com>
2020-04-20 21:41:56 -0300

    Configuring React Navigation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
-
+import {SafeAreaView, Text} from 'react-native';
 export default function App() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Hello World</Text>
-    </View>
+    </SafeAreaView>
   );
 }
