325d5c1de7350f41dda49cbfdb10946faf70ce46
camposLuis <luis.campos.henrique@hotmail.com>
2020-04-20 22:47:41 -0300

    Configuring Bottom tabs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Navigation from './Navigation';
+
 export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hello World</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
   );
 }
